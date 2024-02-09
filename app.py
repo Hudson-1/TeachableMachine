7509316b710ec595edc665c63b851d56fb78a40a
Hudson <70992329+Hudson-1@users.noreply.github.com>
2024-02-09 12:41:12 -0800

    good progress

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+    return render_template("index.html")
+
+@app.route('/time')
+def timer():
+    return render_template("countdown.html")
+
+@app.route('/endscreen')
+def endscreen():
+    return render_template("endscreen.html")
 
 
 if __name__ == '__main__':
     app.run()
-## test
+
---
+++ static/timer.js
@@ -0,0 +1,21 @@
+function startCountdown(duration, display) {
+  var timer = duration, minutes, seconds;
+  var countdownInterval = setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = seconds;
+
+    if (--timer < 0) {
+      clearInterval(countdownInterval); // Stop the countdown
+      window.location.href = "/endscreen"; // Redirect to the end screen
+    }
+  }, 1000);
+}
+
+var timerDisplay = document.getElementById('timer');
+var duration = 29; // Adjusted duration to 30 seconds
+startCountdown(duration, timerDisplay);
